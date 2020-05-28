197f7c8306a3e3411db459a313f2b21c71dc1693
Temmie <25546@ma-web.nl>
2020-05-28 19:57:55 +0200

    Ambient sound added
    Tot nu toe alleen hoesten van de civilians. Komen nog meer geluiden aan!

---
+++ Assets/Dev/Scripts/Sounds/Sound.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Sound : MonoBehaviour
+{
+    [SerializeField] private AudioClip _clip;
+    [SerializeField] private bool _ambient = false;
+    [SerializeField] private float _waitForNextAmbientSound = 120;
+    private AudioSource _source;
+
+    private void Awake()
+    {
+        _source = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        if (!_ambient) StartCoroutine("PlaySound");
+        else
+        {
+            IEnumerator ambient = PlaySound(_waitForNextAmbientSound);
+            StartCoroutine(ambient);
+        }
+    }
+
+    private IEnumerator PlaySound(float time)
+    {
+        while (true)
+        {
+            new WaitForSeconds(time);
+            _source.clip = _clip;
+            _source.Play();
+        }
+    }
+
+    private IEnumerator PlayRepeatingSound()
+    {
+        while (true)
+        {
+            if (!_clip) break;
+            _source.clip = _clip;
+            _source.Play();
+            yield return new WaitUntil(() => !_source.isPlaying);
+
+            AudioClip[] audioclips = GetComponent<SoundTemplate>().GetSoundClip();
+            _clip = audioclips[Random.Range(0, audioclips.Length)];
+        }
+    }
+
+
+
+    #region Getters & Setters
+    public void SetPitch(float value) => _source.pitch = value;
+    public void SetVolume(float value) => _source.volume = value;
+    public void SetClip(AudioClip clip) => _clip = clip;
+    #endregion
+
+
+}
